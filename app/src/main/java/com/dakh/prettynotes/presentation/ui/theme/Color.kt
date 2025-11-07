3ac0774a0dc96f4f9d336deb80369b63cd338333
DmitryAkh <165409513+DmitryAkh@users.noreply.github.com>
2025-11-07 18:23:37 +0300

    Подготовлены цвета и тема

--- app/src/main/java/com/dakh/prettynotes/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/dakh/prettynotes/presentation/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.dakh.prettynotes.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val White = Color(0xFFFFFFFF)
+
+val YellowDark = Color(0xFFB78543)
+val BlueDark = Color(0xFF385277)
+val GreenDark = Color(0xFF3E6653)
+val PurpleDark = Color(0xFF6C4F6B)
+
+val Grey800 = Color(0xFF232225)
+val Grey900 = Color(0xFF181819)
+val Grey100 = Color(0xFFF9F9F9)
+val Grey200 = Color(0xFF868484)
+val Grey300 = Color(0xFF3D3B3B)
+
+val Brown = Color(0xFF703410)
+val BrownDark = Color(0xFF482003)
+
+val PinnedNotesColors = listOf(YellowDark, PurpleDark, BlueDark)
+val OtherNotesColors = listOf(BrownDark, GreenDark, BlueDark)
